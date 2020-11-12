e3bc0db1b57dce9ab906d795beb13a9e40cc1b71
samturton2 <STurton@spartaglobal.com>
2020-11-12 21:52:36 +0000

    task completed

--- main.py
+++ main.py
@@ -22,14 +22,16 @@ class MoviesDatabase:
             reader = csv.reader(csvfile)
             for row in reader:
                 self.movies_list.append(row)
+        # get rid of first 3 letters in movies titleType heading name manually
+        self.movies_list[0][0] = self.movies_list[0][0][3:]
         return self.movies_list
 
-    # Search movies list on title
+    # Search movies list on title, bare in mind read_cvs module has to be called first
     def search_title(self):
         title = input("What movie do you want? : ")
         try:
             for movies in self.movies_list:
-                if movies[2] == title:
+                if movies[2].lower() == title.lower().strip():
                     return movies
             raise AttributeError('Title not in this movie list')
         except:
@@ -46,21 +48,60 @@ class MoviesDatabase:
         while True:
             row = self.search_title()
             # for each column in the row add to the Movies table
-            for num in range(len(self.movies_list[0])):
+            for _ in range(len(self.movies_list[0])):
                 self.cursor.execute(f"""
-                    INSERT INTO Movies ({self.movies_list[num]})
-                    VALUES ('{row[num]}');
+                    INSERT INTO Movies ({self.movies_list[_]})
+                    VALUES ('{row[_]}');
                     """)
             check = input("add another movie ot Movies? ")
-            if check[0].lower() = 'n':
+            if check[0].lower() == 'n':
                 break
 
+    # define a method that adds the whole table to the db, BEWARE OF DUBLICATES
+    def add_all_movies_to_db(self):
+        for row in self.movies_list[1:]:
+            # for each column in the row add to the Movies table
+            for _ in range(len(self.movies_list[0])):
+                self.cursor.execute(f"""
+                    INSERT INTO Movies ({self.movies_list[_]})
+                    VALUES ('{row[_]}');
+                    """)
+
+    # Define a method that loads the db into an object (made abstract) (enter Movies as an argument)
+    def load_object_from_db(self, table):
+        try:
+            table = self.cursor.execute(f"SELECT * FROM {table}").fetchall()
+            return table
+        except:
+            print("please enter a table from northwind db")
+
+    # Define a method that can output a table object to text file line by line (will be in a string format)
+    def write_to_textfile(self, table):
+        with open("imported_table.txt", 'w+') as txtfile:
+            for line in table:
+                txtfile.write(", ".join(str(word) for word in line) + "\n")
 
 
+# Now exectute functions in order of task
+if __name__ == "__main__":
+    ## ITERATION 1
+    # get all the movies
+    obj = MoviesDatabase('SA','Passw0rd2018') # establish connection and make class
+    movies_list = obj.read_csv()
 
-movies_list = []
-with open("imdbtitles.csv", 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        movies_list.append(row)
-print(movies_list)
+    # Search the movie by title
+    print(obj.search_title())
+
+    # Add whatever movies to the db
+    obj.create_table()
+    obj.add_movies()
+
+    ## ITERATION 2
+    #Save whole obj to db
+    obj.add_all_movies_to_db()
+
+    # load from db to obj
+    new_obj = obj.load_object_from_db()
+
+    #output object to text file
+    obj.write_to_textfile(new_obj)
